7a6b6bf104184b16c46a4a1043bfc6818b893c57
Buki89 <ondrej.bukartyk@gmail.com>
2020-09-07 11:30:20 +0200

    working solution 8/8

--- src/App.js
+++ src/App.js
@@ -1,37 +1,53 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import marked from "marked";
 import { mockData } from "./mockData";
 
 function App() {
-  const [editor, setEditor] = useState(mockData);
+  const [editor, setEditor] = useState("");
   const [text, setText] = useState("");
 
+  const renderer = new marked.Renderer();
+
+  marked.setOptions({
+    breaks: true,
+  });
+
+  renderer.link = function (href, title, text) {
+    return `<a target="_blank" href="${href}">${text}` + "</a>";
+  };
+
+  useEffect(() => {
+    setEditor(mockData);
+    setText(marked(editor));
+  }, []);
+
   const handleChange = (e) => {
-    setText(marked(e.target.value));
+    setEditor(e.target.value);
   };
   return (
     <div>
-      <div className='header'>FCC Markdown Previewer</div>
+      <div className="header">FCC Markdown Previewer</div>
 
-      <div className='box'>
-        <div className='container'>
-          <div className='label'>Editor</div>
+      <div className="box">
+        <div className="container">
+          <div className="label">Editor</div>
           <textarea
-            id='editor'
-            className='editor'
+            id="editor"
+            className="editor"
             onChange={handleChange}
-            placeholder={mockData}
+            value={editor}
+            type="text"
           ></textarea>
         </div>
 
-        <div className='container-preview'>
-          <div className='label'>Preview</div>
+        <div className="container-preview">
+          <div className="label">Preview</div>
           <div
-            id='preview'
-            className='preview'
+            id="preview"
+            className="preview"
             dangerouslySetInnerHTML={{
-              __html: marked(`${text}`),
+              __html: marked(`${editor}`, { renderer: renderer }),
             }}
           />
         </div>
